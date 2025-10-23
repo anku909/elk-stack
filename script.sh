cac8f29e4569ad14a8e7c1aead442a91c2a672c6
Ankit Kumar <ankuofficial909@gmail.com>
2025-10-23 15:44:44 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+BASE_URL="https://nxt-logs.sprintnxt.in/livelogs/payments.sprintnxt.in"
+TODAY=$(date +%F)
+DIR="$BASE_URL/$TODAY/payout-decrypted-data"
+
+for i in {10..1}; do
+  URL="$DIR/log_${i}.txt"
+  if curl --output /dev/null --silent --head --fail "$URL"; then
+    echo "$URL"
+    break
+  fi
+done
+
+
+/logs #main log dir
+ /2025-10-01 # inside main log dir folder are created date wise
+ /2025-10-02
+   /sprintopn # after selecting date wise folder inside that there are services folder
+    /irctc 
+    /aeps 
+    /dmt 
+    /recharege
+    /billpay
+        logs.log
+   /sprintverify
+      /pennydrop  # inside service folder there are multiple apis folder 
+      /pennyless
+      /iplookupapi
+      /panverify
+      /adharverify 
+         logs.log # inside api folder there are log files 
